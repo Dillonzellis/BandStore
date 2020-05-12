4b053afe5559211a354e2c38516364e1bd14b481
Dillonzellis <dillonzellis@gmail.com>
2020-05-12 17:14:43 -0400

    Started store.js/remove btn and update total functionality

---
+++ store.js
@@ -0,0 +1,15 @@
+var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+
+for(var i = 0;i < removeCartItemButtons.length; i++){
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function(event) {
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+        updateCartTotal()
+    })
+}
+
+function updateCartTotal(){
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+}
